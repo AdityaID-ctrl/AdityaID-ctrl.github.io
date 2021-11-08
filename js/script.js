79ee924e6541369bea8271005754f41faf231fa5
Aditya Hardi <70373234+AdityaID-ctrl@users.noreply.github.com>
2021-11-08 18:00:49 +0700

    update script

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-// event link klik
+// event pada saat link di klik
 $('.page-scroll').on('click', function(e){
-	
 
-	// ambil isi href
-	var tujuan = $(this).attr('href');
-	// elemen bersangkutan
-	var elemenTujuan = $(tujuan);
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen ybs
+    var elemenTujuan =$(tujuan);
+    
+    // pindahkan scroll
+    $('html, body').animate({
+    	scrollTop: elemenTujuan.offset().top - 50
+    }, 1250, 'easeInOutExpo');
 
-	// pindah scroll
-	$('body').animate({
-		scrollTop: elemenTujuan.offset().top
-	});
-
-	
+    e.preventDefault();
 });
