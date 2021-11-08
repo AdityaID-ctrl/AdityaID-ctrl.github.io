30b605353f0ac1cbc34b2ed5a9bd6422da2b7d79
Aditya Hardi <70373234+AdityaID-ctrl@users.noreply.github.com>
2021-11-08 19:29:42 +0700

    Update Important

--- js/script.js
+++ js/script.js
@@ -18,9 +18,33 @@ $('.page-scroll').on('click', function(e){
 
 
 //parallax
+
 $(window).scroll(function() {
     var wScroll = $(this).scrollTop();
-    console.log(wScroll);
+
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+    });
+
+
+    //portfolio
+    if(wScroll > $('.portfolio').offset().top - 250 ) {
+        $('.portfolio .thumbnail').each(function(i) {
+            setTimeout(function() {
+                $('.portfolio .thumbnail').eq(i).addClass('muncul');
+            }, 300 * i);
+        });
+        
+    }
+    
 });
 
 
